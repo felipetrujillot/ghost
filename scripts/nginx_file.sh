964b1310ea7f13bcfbd6e5a711c4232b3dfd8279
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2024-05-03 21:59:01 -0400

    e

--- scripts/nginx_file.sh
+++ scripts/nginx_file.sh
@@ -53,7 +53,7 @@ if [ -e "/etc/nginx/sites-available/$file_name" ]; then
 fi
 
 # Create the file and add content
-echo "$file_content" | sudo tee "/etc/nginx/sites-available/$file_name" > /dev/null
+echo "$file_content" | tee "/etc/nginx/sites-available/$file_name" > /dev/null
 
 #
 ln -s "/etc/nginx/sites-available/$file_name" "/etc/nginx/sites-enabled/$file_name" 
