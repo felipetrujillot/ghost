f234171589e5dfdfb84ef95598e772a54c56a06a
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2024-05-03 23:31:29 -0400

    db

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,11 +2,10 @@
 
 echo "-------------"
 echo "ghost"
-echo "This script will install everything for you."
 echo "-------------"
 
 apt update -y 
-apt install -y curl wget git nginx
+apt install -y curl wget git nginx php unzip php-curl php-mysqli php-simplexml
 
 echo "Node 20"
 
@@ -18,3 +17,14 @@ npm install pm2 -g
 git config --global user.email "trujillo.felipe1997@gmail.com"
 git config --global user.name "Felipe"
 git config --global pull.rebase false 
+
+echo "composer"
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
+cd /var/wwww
+composer create-project phpmyadmin/phpmyadmin
---
+++ scripts/nginx_db.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+file_name="db.ghost.conf"
+
+file_content='server {
+
+    charset utf-8;
+    server_name db.felipetrujillo.cl;
+
+    gzip on;
+    gzip_disable "msie6";
+    gzip_vary on;
+    gzip_proxied any;
+    gzip_comp_level 6;
+    gzip_buffers 16 8k;
+    gzip_http_version 1.1;
+    gzip_min_length 256;
+
+    gzip_types
+    application/atom+xml
+    application/geo+json
+    application/javascript
+    application/x-javascript
+    application/json
+    application/ld+json
+    application/manifest+json
+    application/rdf+xml
+    application/rss+xml
+    application/xhtml+xml
+    application/xml
+    font/eot
+    font/otf
+    font/ttf
+    image/svg+xml
+    text/css
+    text/javascript
+    text/plain
+    text/xml;
+
+
+    location /phpmyadmin {
+        root /var/www/;
+        index index.php index.html index.htm;
+        location ~ ^/phpmyadmin/(.+.php)$ {
+            try_files $uri =404;
+            fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+            fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+            fastcgi_index index.php;
+            include /etc/nginx/fastcgi_params;
+        }
+
+        location ~* ^/phpmyadmin/(.+.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt))$ {
+            root /var/www;
+        }
+    }
+
+
+    error_log  /var/log/nginx/db-error.log;
+    access_log /var/log/nginx/db.log;
+}'
+
+if [ -e "/etc/nginx/sites-available/$file_name" ]; then
+    echo "File already exists."
+    exit 1
+fi
+
+# Create the file and add content
+echo "$file_content" | tee "/etc/nginx/sites-available/$file_name" > /dev/null
+
+#
+ln -s "/etc/nginx/sites-available/$file_name" "/etc/nginx/sites-enabled/$file_name" 
+
+
+systemctl restart nginx
